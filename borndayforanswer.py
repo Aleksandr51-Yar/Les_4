620b033678460045a8fe2dff9ea871711d79d0e7
Aleksandr51-Yar <alvasop2@gmail.com>
2024-08-03 15:12:32 +0300

    add file

---
+++ borndayforanswer.py
@@ -0,0 +1,17 @@
+def ask_year():
+    year = input('Ввведите год рождения А.С.Пушкина:')
+    while year != '1799':
+      print("Не верно")
+      year = input('Ввведите год рождения А.С.Пушкина:')
+
+def ask_day():
+    day = input('Ввведите день рождения Пушкин?')
+    while day != '6':
+       print("Не верно")
+       day = input('В какой день июня родился Пушкин?')
+print('Верно')
+
+ask_year()
+ask_day()
+ask_year()
+print('Верно')
